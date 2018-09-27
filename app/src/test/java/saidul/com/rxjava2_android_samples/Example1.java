d906a444b1bc141b4a62e37f730e927f041ad46a
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-09-27 22:58:33 +0600

    Initilcaiton project

---
+++ app/src/test/java/saidul/com/rxjava2_android_samples/Example1.java
@@ -0,0 +1,14 @@
+package saidul.com.rxjava2_android_samples;
+
+import org.junit.Test;
+
+/**
+ * Created by Kazi Md. Saidul Email: Kazimdsaidul@gmail.com  Mobile: +8801675349882 on 9/27/18.
+ */
+public class Example1 {
+
+    @Test
+    public void  helloTest(){
+        System.out.println("hello");
+    }
+}
